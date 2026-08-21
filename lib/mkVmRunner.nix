c1db004113b91fc18c577e9210665a05866091c9
Daniel Hellstern <git@daniel.hellstern.org>
2026-08-20 17:43:28 -0700

    fix: improved runner builder

--- lib/mkVmRunner.nix
+++ lib/mkVmRunner.nix
@@ -2,17 +2,26 @@
 
 {
   name,
-  vmImage,
-  memMb ? 4096,
+  image,
   cpus ? 2,
-  arch ? "x86_64",
+  mem ? 4096,
+  arch ? pkgs.stdenv.hostPlatform.parsed.cpu.name,
+  machineType ? "i440fx",
+  display ? "vga",
+  kvm ? true,
+  efi ? false,
+  portForwards ? [ ],
   extraArgs ? [ ],
-  useKVM ? true
 }:
 
 let
-  baseImagePath = "${vmImage}/disk.qcow2";
-  extraArgsStr = lib.escapeShellArgs extraArgs;
+  # Accept arbitrary QEMU arguments
+  extraArgsStr = lib.escapeShellArgs (lib.flatten extraArgs);
+
+  # Generate a list of port forwards (if enabled)
+  hostfwdFlags = lib.concatMapStringsSep "," (
+    fwd: "hostfwd=tcp::${toString fwd.host}-:${toString fwd.guest}"
+  ) portForwards;
 in
 pkgs.writeShellScriptBin "run-vm" ''
   # bash
@@ -23,9 +32,13 @@ pkgs.writeShellScriptBin "run-vm" ''
   STATE_DIR="''${XDG_DATA_HOME:-$HOME/.local/share}/dvm/${name}"
   OVERLAY_DISK="$STATE_DIR/overlay.qcow2"
 
-  DISABLE_KVM=${!useKVM}
+  MACHINE_TYPE="${machineType}"
+  VIDEO_MODE="${display}"
+  DISABLE_KVM=${!kvm}
+  ENABLE_EFI=${efi}
+  PORT_FORWARDS="${hostfwdFlags}"
+  EXTRA_QEMU_ARGS=(${lib.escapeShellArgs extraArgs})
   RESET_OVERLAY=false
-  EXTRA_QEMU_ARGS=()
 
   # Parse runtime flags
   while [[ $# -gt 0 ]]; do
@@ -43,10 +56,6 @@ pkgs.writeShellScriptBin "run-vm" ''
         EXTRA_QEMU_ARGS+=("$@")
         break
         ;;
-      *)
-        EXTRA_QEMU_ARGS+=("$1")
-        shift
-        ;;
     esac
   done
 
@@ -62,31 +71,51 @@ pkgs.writeShellScriptBin "run-vm" ''
   # Create a QCOW2 overlay pointing to the immutable base image in store
   if [ ! -f "$OVERLAY_DISK" ]; then
     echo "Creating a copy-on-write overlay disk at $OVERLAY_DISK"
-    ${pkgs.qemu-img}/bin/qemu-img create \
+    ${lib.getExe pkgs.qemu-img} create \
       -f qcow2 \
-      -b "${baseImagePath}" \
+      -b "${image}" \
       -F qcow2 \
       "$OVERLAY_DISK"
   fi
 
   # Acceleration flags check
   ACCEL_FLAGS="-enable-kvm -cpu host"
-  if [ "$DISABLE_KVM" = true ]; then
-    echo "Warning: KVM disabled via CLI flag. This likely will have a significant performance impact."
-    ACCEL_FLAGS="-accel tcg"
-  elif [ ! -w /dev/kvm ]; then
-    echo "Warning: /dev/kvm is not writable. This likely will have a significant performance impact."
+  if [ "$DISABLE_KVM" = true ] || [ ! -w /dev/kvm ]; then
+    echo "Warning: KVM is disabled or unavailable. This will have a significant performance impacts."
     ACCEL_FLAGS="-accel tcg"
   fi
 
+  # Correctly link OVMF for UEFI boot
+  BOOT_FLAGS=""
+  if [ "$ENABLE_EFI" = true ]; then
+    MACHINE_TYPE="q35"
+    BOOT_FLAGS="-bios ${pkgs.OVMF_fd}/FV/OVMF.fd"
+  fi
+
+  # Support SPICE and headless VMs
+  VIDEO_FLAGS="-vga std"
+  if [ "$VIDEO_MODE" = "qxl" ]; then
+    VIDEO_FLAGS="-vga qxl -spice port=5900,disable-ticketing"
+  elif [ "$VIDEO_MODE" = "none" ]; then
+    VIDEO_FLAGS="-vga std -display none"
+  fi
+
+  # Port-forwarding flags are generated statically by Nix at eval time.
+  NETDEV_FLAGS="-netdev user,id=net0"
+  if [ "$PORT_FORWARDS" != "" ]; then
+    NETDEV_FLAGS+=",${PORT_FORWARDS}"
+  fi
+  NETDEV_FLAGS+=" -device virtio-net-pci,netdev=net0"
+
   echo "Launching Virtual Machine ${name}..."
   exec ${lib.getExe' pkgs.qemu_kvm "qemu-system-" + arch} \
-    $ACCEL_FLAGS \
     -m ${toString memMb} \
     -smp ${toString cpus} \
+    -M ''${MACHINE_TYPE} \
     -drive file="$OVERLAY_DISK",format=qcow2,if=virtio \
-    -netdev user,id=net0,hostfwd=tcp::2222-:22 \
-    -device virtio-net-pci,netdev=net0 \
-    ${extraArgsStr} \
+    ''${ACCEL_FLAGS} \
+    ''${BOOT_FLAGS} \
+    ''${VIDEO_FLAGS} \
+    ''${NETDEV_FLAGS} \
     "''${EXTRA_QEMU_ARGS[@]}"
 ''
